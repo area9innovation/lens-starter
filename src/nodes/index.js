7f4b3aa27280ba0d351af9509cc18c6cb27f6765
corneyy <corneyy@gmail.com>
2017-05-25 19:27:16 +0400

    FIX active links for figures and tables in one column mode

--- src/nodes/index.js
+++ src/nodes/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   'figure_group': require('./figure_group'),
-  'figure_reference': require('./figure_reference'),
   'citation': require('./citation'),
   'html_table': require('./html_table'),
   'figure': require('./figure'),
