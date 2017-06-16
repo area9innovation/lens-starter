b10adc5f97011bb26bf31636dbecdd57d4fa1e10
corneyy <corneyy@gmail.com>
2017-06-16 21:54:55 +0400

    Support for IE 11

--- boot.js
+++ boot.js
@@ -1,5 +1,31 @@
 window.Lens = require("./src/my-lens");
 
+if (!String.prototype.includes) {
+  String.prototype.includes = function() {
+    'use strict';
+    return String.prototype.indexOf.apply(this, arguments) !== -1;
+  };
+}
+
+(function (constructor) {
+    if (constructor &&
+        constructor.prototype &&
+        constructor.prototype.children == null) {
+        Object.defineProperty(constructor.prototype, 'children', {
+            get: function () {
+                var i = 0, node, nodes = this.childNodes, children = [];
+                //iterate all childNodes
+                while (node = nodes[i++]) {
+                    //remenber those, that are Node.ELEMENT_NODE (1)
+                    if (node.nodeType === 1) { children.push(node); }
+                }
+                return children;
+            }
+        });
+    }
+  //apply the fix to all HTMLElements (window.Element) and to SVG/XML (window.Node)
+})(window.Node || window.Element);
+
 // Little helper used to parse query strings from urls
 // --------
 //
