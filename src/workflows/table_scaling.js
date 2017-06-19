0ff2a073eb1aec487c6eca3aa47bfdd10af053e4
corneyy <corneyy@gmail.com>
2017-06-19 14:04:34 +0400

    FIX for ie 11

--- src/workflows/table_scaling.js
+++ src/workflows/table_scaling.js
@@ -23,12 +23,16 @@ TableScaling.Prototype = function() {
   this.handleStateUpdate = function(state, stateInfo) {
     ++this.pass;
     if ( this.pass > 1 ) return;
+    var ie = this.detectIE()
 
     if ( !$('#forlens').length ) {
       $('body *').not('.video-wrapper *').css('position', 'unset');
       $('body').css('position', 'unset');  
       $('body').css('overflow', 'auto');  
-      $('#container').css('height', 'auto');  
+      if( ie && ie<=11) {}
+      else {
+        $('#container').css('height', 'auto');  
+      }
     }
 
     var popup = $('<div id="popup" class="lens-article"></div>').css({
@@ -219,6 +223,46 @@ TableScaling.Prototype = function() {
   this.DoScroll = function(store) {
     store.prevScroll = store.scroll;
     store.scroll = $(window).scrollTop();
+  };
+
+  this.detectIE = function() {
+    var ua = window.navigator.userAgent;
+
+    // Test values; Uncomment to check result …
+
+    // IE 10
+    // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+    
+    // IE 11
+    // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+    
+    // Edge 12 (Spartan)
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+    
+    // Edge 13
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+      // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      // Edge (IE 12+) => return version number
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
   };
 };
 
