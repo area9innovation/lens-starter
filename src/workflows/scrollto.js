119d317a2242a1da5fb476e21e694ee9c1b11921
corneyy <corneyy@gmail.com>
2017-11-02 22:41:38 +0400

    Safari mobile "scroll to video" support

--- src/workflows/scrollto.js
+++ src/workflows/scrollto.js
@@ -27,7 +27,6 @@ ScrollToReference.Prototype = function() {
           if($('.spinner-wrapper').css('display') == 'none') {
             clearInterval(timerId);
             that.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
-            console.log('none');
           }
         }, 100);
 
@@ -35,37 +34,21 @@ ScrollToReference.Prototype = function() {
 
         var handcarWorker = function() {
           var imageFillin = this;
-//          if( imageFillin.complete ) return;
 
           $(imageFillin).hide();
 
           var deferred = $.Deferred();
-
           ds.push(deferred);
-/*
-          deferred.always(function(){
-            $(imageFillin).show();
-            if($('.spinner-wrapper').css('display') == 'none') {
-              console.log('defer1');
-              _.defer(function()
-                {
-                  that.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
-                  console.log('defer');
-                });
-            }
-          });
-*/
+
           $(imageFillin).one('load', deferred.resolve);
         };
 
         $('.surface.resource-view.content .image-wrapper img').each(handcarWorker);
 
         $.when.apply($, ds).then(function(){
-          console.log('when1');
           $('.surface.resource-view.content .image-wrapper img').show();
           if($('.spinner-wrapper').css('display') == 'none') {
             that.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
-            console.log('when');
           }
         });
       }
