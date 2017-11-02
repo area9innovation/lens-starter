d0ead6e9f82e2019f32fa9291db73e9c08e313b2
corneyy <corneyy@gmail.com>
2017-11-02 22:40:38 +0400

    Safari mobile "scroll to video" support

--- src/workflows/scrollto.js
+++ src/workflows/scrollto.js
@@ -19,7 +19,56 @@ ScrollToReference.Prototype = function() {
 
   this.handleStateUpdate = function(state, stateInfo) {
     if ( stateInfo.focussedNode ) {
-      this.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
+      if( navigator.vendor.indexOf('Apple') == -1) {
+        this.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
+      } else {
+        var that = this;
+        var timerId = setInterval(function() {
+          if($('.spinner-wrapper').css('display') == 'none') {
+            clearInterval(timerId);
+            that.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
+            console.log('none');
+          }
+        }, 100);
+
+        var ds = [];
+
+        var handcarWorker = function() {
+          var imageFillin = this;
+//          if( imageFillin.complete ) return;
+
+          $(imageFillin).hide();
+
+          var deferred = $.Deferred();
+
+          ds.push(deferred);
+/*
+          deferred.always(function(){
+            $(imageFillin).show();
+            if($('.spinner-wrapper').css('display') == 'none') {
+              console.log('defer1');
+              _.defer(function()
+                {
+                  that.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
+                  console.log('defer');
+                });
+            }
+          });
+*/
+          $(imageFillin).one('load', deferred.resolve);
+        };
+
+        $('.surface.resource-view.content .image-wrapper img').each(handcarWorker);
+
+        $.when.apply($, ds).then(function(){
+          console.log('when1');
+          $('.surface.resource-view.content .image-wrapper img').show();
+          if($('.spinner-wrapper').css('display') == 'none') {
+            that.readerView.contentView.findNodeView(stateInfo.focussedNode.id).scrollIntoView();
+            console.log('when');
+          }
+        });
+      }
     }
     return false;
   };
