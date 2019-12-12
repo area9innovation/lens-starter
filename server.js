57ffe1714a66f62c5525aab59fb4c282f373f3dc
lyazdanov <lenar.yazdanov@area9.dk>
2019-12-12 11:02:51 +0500

    fixed packaging problem with express: Most middleware (like bodyParser) is no longer bundled with Express

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var _ = require("underscore");
 var fs = require('fs');
 var path = require("path");
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var sass = require('node-sass');
 var browserify = require('browserify');
@@ -11,9 +14,9 @@ var browserify = require('browserify');
 var app = express();
 
 var port = process.env.PORT || 4001;
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(methodOverride());
 
 
 app.get('/lens.js', function (req, res, next) {
@@ -63,7 +66,6 @@ app.use(express.static(path.join(__dirname, 'assets')));
 // Serve Lens in dev mode
 // --------
 
-app.use(app.router);
 
 http.createServer(app).listen(port, function(){
   console.log("Lens running on port " + port);
