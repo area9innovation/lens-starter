2e39ac085196285b3d868ba17cf77fd7ea59d967
Alexander Kolodezhnyi <diamond_system@mail.ru>
2019-12-16 22:34:34 +0800

    Fix eLetter Submit

--- src/jbjs_converter.js
+++ src/jbjs_converter.js
@@ -757,14 +757,18 @@ JbjsConverter.Prototype = function() {
   };
 
   this.eLetterSubmit = function(state, oneColumnArticle) {
-    var doc = state.doc;
+    var doc = state.doc,
+        url = window.location.href,
+        i = url.indexOf('#');
+
+    if (i !== -1) url = url.substring(0, i);
 
     var eletterSubmitNode = {
       id: 'eletters',
       source_id: null,
       type: 'eletter_submit',
       label: 'Submit an eLetter',
-      url: 'http://eletters.jbjs.org/?r=' + encodeURIComponent(window.location.href),
+      url: 'http://eletters.jbjs.org/?r=' + encodeURIComponent(decodeURIComponent(url)),
       caption: oneColumnArticle ? 'eLetters' : null,
       image: this.ResourceURLBuilder('Letter_to_Editor_Widget_Icon_Small.png')
     };
